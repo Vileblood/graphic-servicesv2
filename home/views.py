7d726b9c8b6cefb5ea2313e77c7815292cb11207
Vileblood <david.thomas1986@gmail.com>
2020-07-29 17:37:58 +0000

    add blog app

--- home/views.py
+++ home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 def index(request):
     """A view to return the index page """
     return render(request, 'home/index.html')
+
+def addblog(request):
+    """A view to return add blog page"""
+    return render(request, 'addblog/addblog.html')
+
+
